6d8980f94bcfe0f0e8507a2d1a43e83e9f766e6e
ndduc <ndduc1856@gmail.com>
2020-06-03 15:37:34 -0700

    tested youtube image population

--- Android_rebuilt/app/src/main/java/ndduc/project/musicplayer/activity_Populate.java
+++ Android_rebuilt/app/src/main/java/ndduc/project/musicplayer/activity_Populate.java
@@ -14,13 +14,16 @@ import com.squareup.picasso.Picasso;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.List;
+
+import ndduc.project.musicplayer.Container.YoutubeData;
+import ndduc.project.musicplayer.Json_Handler.Json_Decoder;
 
 public class activity_Populate extends AppCompatActivity {
 
     TextView txtApi, txtTitle, txtNum;
     Button btnSearch;
     TableLayout layoutTable;
-    ImageView ivImage_1;
 
     protected void onCreate(Bundle savedInstance) {
         super.onCreate(savedInstance);
@@ -40,14 +43,38 @@ public class activity_Populate extends AppCompatActivity {
 
         btnSearch = findViewById(R.id.btnSearch);
 
-        ivImage_1 = findViewById(R.id.imgView_1);
+        layoutTable = findViewById(R.id.layoutTable);
     }
 
     private void populateImage() throws Exception {
 
-        Picasso.get().load("https://i.ytimg.com/vi/rj7xMBxd5iY/default.jpg").into(ivImage_1);
+        List<YoutubeData> lst = testYoutubePopulate();
 
+        for (int r = 0; r < lst.size(); r++){
+          //  URL img = (URL) lst.get(r).getImage();
+            TableRow tr = new TableRow(this);
+            for (int c= 0 ; c < 1; c++){
+                ImageView im = new ImageView (this);
+                Picasso.get().load(lst.get(r).getImage().toString()).into(im);
+                im.setPadding(20, 0, 10, 0); //padding in each image if needed
+                //add here on click event etc for each image...
+                //...
+                tr.addView(im, 300,300);
+            }
+            layoutTable.addView(tr);
+        }
 
+    }
 
+    private List<YoutubeData>  testYoutubePopulate () {
+        try {
+            // JSONObject json = URL_Decoder.readYoutube("AIzaSyDE2igQOTyQ6XgJM03wLazUBF_zmWYWx4Q", "unravel", "1", "DETAIL");
+            // Debug.debug("RESULT", json);
+            List<YoutubeData> lst = Json_Decoder.getJsonContent("AIzaSyDE2igQOTyQ6XgJM03wLazUBF_zmWYWx4Q", "unravel", "5", "DETAIL");
+            return lst;
+        }  catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
